144c65e6cb1aee096e949fdc7567e77b566ab93d
Alexander Logunov <dswildy@gmail.com>
2019-05-13 18:59:57 +0500

    feat: рефакторинг

--- app/src/main/java/alexander/logunov/contacts/view_model/ContactListViewModel.kt
+++ app/src/main/java/alexander/logunov/contacts/view_model/ContactListViewModel.kt
@@ -9,7 +9,6 @@ import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -48,7 +47,11 @@ class ContactListViewModel(application: Application) : AndroidViewModel(applicat
             Log.w(TAG, t)
             isLoading.postValue(false)
             isRefreshing.postValue(false)
-            Toast.makeText(getApplication(), "Ошибка сети", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                getApplication(),
+                "Ошибка сети",
+                Toast.LENGTH_LONG
+            ).show()
         }
 
         override fun onResponse(call: Call<List<Contact>>, response: Response<List<Contact>>) {
@@ -67,9 +70,7 @@ class ContactListViewModel(application: Application) : AndroidViewModel(applicat
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                    {
-                        Log.d(TAG, "Contacts saved to DB")
-                    },
+                    { Log.d(TAG, "Contacts saved to DB") },
                     { error -> Log.e(TAG, "Unable to save contacts", error) }
                 )
         )
@@ -84,14 +85,24 @@ class ContactListViewModel(application: Application) : AndroidViewModel(applicat
                 {
                     contactsList.addAll(it)
                     contacts.postValue(contactsList)
-                    if (it.count() > 1 && Date(it.last().createdAt + 1000 * 60).after(Date())) {
-                        Toast.makeText(getApplication(), "Загрузил из БД, дата создания последнего: ${Date(it.last().createdAt)}", Toast.LENGTH_LONG).show()
+                    val count = it.count()
+                    val last = it.last()
+                    if (count > 1 && Date(last.createdAt + 1000 * 60).after(Date())) {
+                        Toast.makeText(
+                            getApplication(),
+                            "Загрузил из БД ${count} контактов, дата создания последнего: ${Date(last.createdAt)}",
+                            Toast.LENGTH_LONG
+                        ).show()
                         isLoading.postValue(false)
                         isRefreshing.postValue(false)
                         Log.d(TAG, "Contacts loaded from DB")
                     } else {
                         Log.d(TAG, "Loading from GitHub")
-                        Toast.makeText(getApplication(), "Загружаю из гитхаба, контактов в БД: ${it.count()}", Toast.LENGTH_LONG).show()
+                        Toast.makeText(
+                            getApplication(),
+                            "Обновляю БД, было ${count} записей",
+                            Toast.LENGTH_LONG
+                        ).show()
                         loadContacts()
                     }
 
@@ -103,6 +114,8 @@ class ContactListViewModel(application: Application) : AndroidViewModel(applicat
     fun loadContacts() {
         isLoading.postValue(true)
         Api.getInstance().getContacts(1, handleContactsLoad)
+        Api.getInstance().getContacts(2, handleContactsLoad)
+        Api.getInstance().getContacts(3, handleContactsLoad)
     }
 
     fun filterByNameOrPhone(query: String)  {
